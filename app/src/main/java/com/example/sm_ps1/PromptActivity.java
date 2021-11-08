0eaad635bdfe22a9d40435ad16c4aac92fb2d234
ErykGodles <eryk161@gmail.com>
2021-11-08 12:30:48 +0100

    init

---
+++ app/src/main/java/com/example/sm_ps1/PromptActivity.java
@@ -0,0 +1,41 @@
+package com.example.sm_ps1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class PromptActivity extends AppCompatActivity {
+    private boolean correctAnswer;
+    private Button showAnswerButton;
+    private TextView correctAnswerTextView;
+    public static final String KEY_EXTRA_ANSWER_SHOWN = "answerShown";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prompt);
+        correctAnswer = getIntent().getBooleanExtra(MainActivity.KEY_EXTRA_ANSWER, true);
+
+        showAnswerButton = findViewById(R.id.show_button);
+        correctAnswerTextView = findViewById(R.id.textViewAnswer);
+
+        showAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int answer = correctAnswer ? R.string.button_true : R.string.button_false;
+                correctAnswerTextView.setText(answer);
+                setAnswerShownResult(true);
+            }
+        });
+
+    }
+    private void setAnswerShownResult(boolean answerWasShown){
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(KEY_EXTRA_ANSWER_SHOWN, answerWasShown);
+        setResult(RESULT_OK, resultIntent);
+
+    }
+}
